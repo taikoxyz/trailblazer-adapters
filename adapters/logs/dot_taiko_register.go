4f2354d134d765f27da44c957307e32612bc5685
ben <bennettlikebeckham@gmail.com>
2024-07-17 09:54:55 +0100

    feat: ens

---
+++ adapters/logs/dot_taiko_register.go
@@ -0,0 +1,60 @@
+package logs
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/taikoxyz/trailblazer-adapters/adapters"
+)
+
+var _ adapters.TransferLogsIndexer = (*DotTaikoIndexer)(nil)
+
+var (
+	logMintDomainSigHash     = crypto.Keccak256Hash([]byte("MintedDomain(string,uint256,address,uint256)"))
+	logNameRegisteredSigHash = crypto.Keccak256Hash([]byte("NameRegistered(uint256,string,bytes32,address,uint256,uint256)"))
+)
+
+type DotTaikoIndexer struct {
+	Addresses []common.Address
+}
+
+func NewDotTaikoIndexer(addresses []common.Address) *DotTaikoIndexer {
+	return &DotTaikoIndexer{Addresses: addresses}
+}
+
+func (indexer *DotTaikoIndexer) IndexLogs(ctx context.Context, chainID *big.Int, client *ethclient.Client, logs []types.Log) ([]adapters.TransferData, error) {
+	var result []adapters.TransferData
+	for _, vLog := range logs {
+		if !indexer.isRelevantLog(vLog.Topics[0]) {
+			continue
+		}
+		transferData, err := indexer.ProcessLog(ctx, chainID, client, vLog)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, *transferData)
+	}
+	return result, nil
+}
+
+func (indexer *DotTaikoIndexer) isRelevantLog(topic common.Hash) bool {
+	return topic.Hex() == logNameRegisteredSigHash.Hex() || topic.Hex() == logMintDomainSigHash.Hex()
+}
+
+func (indexer *DotTaikoIndexer) ProcessLog(ctx context.Context, chainID *big.Int, client *ethclient.Client, vLog types.Log) (*adapters.TransferData, error) {
+	ownerHex := common.BytesToAddress(vLog.Topics[2].Bytes()[12:])
+
+	block, err := client.BlockByNumber(ctx, big.NewInt(int64(vLog.BlockNumber)))
+	if err != nil {
+		return nil, err
+	}
+
+	return &adapters.TransferData{
+		To:   ownerHex,
+		Time: block.Time(),
+	}, nil
+}
--- cmd/logs/logs.go
+++ cmd/logs/logs.go
@@ -7,6 +7,7 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/taikoxyz/trailblazer-adapters/adapters/logs"
 )
@@ -17,7 +18,7 @@ func main() {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	processor := logs.NewTransferIndexer(nil)
+	processor := logs.NewDotTaikoIndexer([]common.Address{common.HexToAddress("0xD7b837A0E388B4c25200983bdAa3EF3A83ca86b7"), common.HexToAddress("0xFb2Cd41a8aeC89EFBb19575C6c48d872cE97A0A5")})
 
 	chainID, err := client.ChainID(context.Background())
 	if err != nil {
@@ -25,8 +26,8 @@ func main() {
 	}
 	query := ethereum.FilterQuery{
 		Addresses: processor.Addresses,
-		FromBlock: big.NewInt(int64(1000)),
-		ToBlock:   big.NewInt(int64(1001)),
+		FromBlock: big.NewInt(int64(178747)),
+		ToBlock:   big.NewInt(int64(178798)),
 	}
 	logs, err := client.FilterLogs(context.Background(), query)
 	if err != nil {
