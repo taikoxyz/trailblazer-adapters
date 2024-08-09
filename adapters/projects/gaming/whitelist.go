3d8021b332fe8ed332f6ff617464fd0c7a44378d
ben <bennettlikebeckham@gmail.com>
2024-08-09 14:17:10 +0800

    refactor: whitelist

--- adapters/projects/gaming/whitelist.go
+++ adapters/projects/gaming/whitelist.go
@@ -9,17 +9,24 @@ import (
 	"github.com/taikoxyz/trailblazer-adapters/adapters"
 )
 
-var (
-	Addresses = map[string][]common.Address{
-		"0xAstra": {
+const (
+	// Define your addresses in a constant
+	astra         = "0xAstra"
+	crackAndStack = "Crack&Stack"
+	looperLands   = "LooperLands"
+	ultiverse     = "Ultiverse"
+	zypherNetwork = "ZypherNetwork"
+	evmWarfare    = "EVMWarfare"
+)
+
+// GetAddresses returns the map of addresses
+func GetAddresses() map[string][]common.Address {
+	return map[string][]common.Address{
+		astra: {
 			common.HexToAddress("0x90CE48ED68C6FCAe6F13b445F1573f003cF1804d"),
 			common.HexToAddress("0x34723B92aE9708BA33843120A86035D049dA7dfA"),
 		},
-		"21Blackjack": {
-			common.HexToAddress("0x8C5720982b54874F53F2514BbD2382ADce98a0ca"),
-			common.HexToAddress("0x78adDA11Bfc437DeC4a39318FF7e52Cf00DC062c"),
-		},
-		"Crack&Stack": {
+		crackAndStack: {
 			common.HexToAddress("0x7ddB8A975778a434dE03dd666F11Ce962DCdD290"),
 			common.HexToAddress("0x2c301eBfB0bb42Af519377578099b63E921515B7"),
 			common.HexToAddress("0x6C8865042792B5E919fC95bF771ccaDF6F0cfA22"),
@@ -30,40 +37,35 @@ var (
 			common.HexToAddress("0xD8F7cd7d919c5266777FB83542F956dD30E80187"),
 			common.HexToAddress("0x009C32F03d6eEa4F6DA9DD3f8EC7Dc85824Ae0e6"),
 		},
-		"LooperLands": {
+		looperLands: {
 			common.HexToAddress("0xEe01C4b0538849bF1c66bDFB458a7de11B1d7424"),
 		},
-		"StupidMonkeys": {
-			common.HexToAddress("0xCA99F9DbF4A13D4de05B41a68041dcE7929cb5e0"),
-		},
-		"SwordsAndDungeons": {
-			common.HexToAddress("0xac67aB5D44d5A7DbB55b8D9d1893889B7DaE3a36"),
-			common.HexToAddress("0xc8b15Aa9BD24961E2ee4f3C67aEA4EafcB071108"),
-			common.HexToAddress("0xEF9bF561ccbCBeDA1A71341A2162e5e6fF28c929"),
-		},
-		"Ultiverse": {
+		ultiverse: {
 			common.HexToAddress("0xa30c146A816931f6134dD8676B9dE8E77496954d"),
 			common.HexToAddress("0xE3b5500039F401e48627e8025b37d4871cF34f36"),
 			common.HexToAddress("0x06F9914838903162515aFa67D5b99Ada0F9791cc"),
 		},
-		"ZypherNetwork": {
+		zypherNetwork: {
 			common.HexToAddress("0xd4629d312CdC663D062F3Fbc322534A9Df0151bC"),
 		},
-		"EVMWarfare": {
+		evmWarfare: {
 			common.HexToAddress("0xA9E8e13BA0309486b2F710B8201B534D26633a89"),
 		},
 	}
-)
+}
 
+// GamingWhitelist stores valid recipients
 type GamingWhitelist struct {
 	ValidRecipient map[string]struct{}
 }
 
+// NewGamingWhitelist initializes a new GamingWhitelist
 func NewGamingWhitelist() *GamingWhitelist {
 	whitelist := &GamingWhitelist{
-		ValidRecipient: map[string]struct{}{},
+		ValidRecipient: make(map[string]struct{}),
 	}
-	for _, addrList := range Addresses {
+	addresses := GetAddresses()
+	for _, addrList := range addresses {
 		for _, addr := range addrList {
 			whitelist.ValidRecipient[addr.Hex()] = struct{}{}
 		}
@@ -71,6 +73,7 @@ func NewGamingWhitelist() *GamingWhitelist {
 	return whitelist
 }
 
+// ProcessBlock processes a block and returns the whitelist entries
 func (indexer *GamingWhitelist) ProcessBlock(ctx context.Context, block *types.Block, client *ethclient.Client) ([]adapters.Whitelist, error) {
 	txs := block.Transactions()
 	var result []adapters.Whitelist
